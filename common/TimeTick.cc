88513e4882d8552009a923044a1d50e567bb8674
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2014-10-13 10:41:59 +0530

    remove duplicate copyright

--- common/TimeTick.cc
+++ common/TimeTick.cc
@@ -1,7 +1,5 @@
 // (c) 2014 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
 // see LICENSE for license
-// copyright (c) 2010-12 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
-// see LICENSE.txt for license information
 #include "TimeTick.hh"
 
 #include <cmath>
