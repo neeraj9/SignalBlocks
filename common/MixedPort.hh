058ef0bb58eb02ffd8c66c8066ec636c244e8686
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-03-11 23:35:20 +0530

    Remove duplicate copyright

--- common/MixedPort.hh
+++ common/MixedPort.hh
@@ -1,7 +1,5 @@
 // (c) 2014 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
 // see LICENSE for license
-// copyright (c) 2010-12 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
-// see LICENSE.txt for license information
 #ifndef sigblocks_common_MixedPort_hh
 #define sigblocks_common_MixedPort_hh
 
