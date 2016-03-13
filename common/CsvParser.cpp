011e82fd16adce83c737141d24462883e9569af6
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-03-13 13:49:36 +0530

    The parser now correctly decodes quoted fields although the implementation is unoptimized and at places pretty ugly. Something to cleanup later.

--- common/CsvParser.cpp
+++ common/CsvParser.cpp
@@ -11,6 +11,96 @@ using namespace sigblocks;
 namespace {
     static std::regex csv_regex(",");
     static std::regex quote_regex("\"");
+
+
+    // split the line within CSV file, which can be quoted with double quotes (")
+    // and fields are separated by comma (,).
+    inline std::vector<std::string> CsvSplit(std::ifstream& instream,
+                                             const std::string& filename,
+                                             char quote = '"',
+                                             char delim = ',') {
+        std::string line;
+        if (! std::getline(instream, line)) {
+            return std::vector<std::string>();
+        }
+        std::vector<std::string> result;
+        std::string leftover;
+        bool done = false;
+        do {
+            unsigned long start_position = 0;
+            unsigned long pos = 0;
+            bool has_quote = false;
+            for (const char* ptr = line.c_str(); *ptr != '\0'; ++ptr) {
+                if (*ptr == quote) {
+                    has_quote = (!has_quote);  // toggle quotes
+                } else if (*ptr == delim) {
+                    if (!has_quote) {
+                        if (! leftover.empty()) {
+                            leftover.append(std::move(line.substr(start_position, pos - start_position)));
+                            if ((leftover[0] == quote) && (leftover[leftover.size() - 1] == quote)) {
+                                result.push_back(leftover.substr(1, line.size() - 2));
+                            } else {
+                                result.push_back(leftover);
+                            }
+                            leftover.clear();
+                        } else {
+                            if ((line[start_position] == quote) && (line[pos - 1] == quote)) {
+                                result.push_back(std::move(line.substr(start_position + 1, pos - start_position - 2)));
+                            } else {
+                                result.push_back(std::move(line.substr(start_position, pos - start_position)));
+                            }
+                        }
+                        start_position = pos + 1;  // skip over the delimiter character
+                    } else {
+                        // ignore the delimiter because its quoted
+                    }
+                }
+                ++pos;
+            }
+            // if open quote then it appears that a field has a newline char.
+            if (has_quote) {
+                if (!std::getline(instream, line)) {
+                    LOG_ERROR("quoted field ends at the last line in file=%s\n", filename.c_str());
+                    return std::vector<std::string>();
+                }
+                std::string tmp;
+                if ((line[start_position] == quote) && (line[pos - 1] == quote)) {
+                    std::string tmp(std::move(line.substr(start_position + 1, pos - start_position - 2)));
+                    leftover.swap(tmp);
+                } else {
+                    std::string tmp(std::move(line.substr(start_position, pos - start_position)));
+                    leftover.swap(tmp);
+                }
+                //std::string tmp(std::move(line.substr(start_position, pos - start_position)));
+                //leftover.swap(tmp);
+            } else {
+                if (start_position < pos) {
+                    // validate the last field because there are cases
+                    // where these are just newline characters
+                    const char *tmp = line.c_str();
+                    bool valid_field = false;
+                    for (unsigned long i = start_position; i < pos; ++i) {
+                        if ((tmp[i] != '\r') && (tmp[i] != '\n')) {
+                            valid_field = true;
+                            break;
+                        }
+                    }
+                    if (valid_field) {
+                        if ((line[start_position] == quote) && (line[pos - 1] == quote)) {
+                            result.push_back(std::move(line.substr(start_position + 1, pos - start_position - 2)));
+                        } else {
+                            result.push_back(std::move(line.substr(start_position, pos - start_position)));
+                        }
+                        //result.push_back(std::move(line.substr(start_position, pos - start_position)));
+                    }
+                }
+                // else nothing to add
+                done = true;
+            }
+        } while(! done);
+
+        return result;
+    }
 }
 
 
@@ -21,16 +111,12 @@ CsvParser::CsvParser(const std::string& filename, bool hasHeader)
           mHeader(),
           mLineNumber(0) {
     if (mHasHeader) {
-        std::string line;
-        if (!std::getline(mInFile, line)) {
+        std::vector<std::string> fields(std::move(CsvSplit(mInFile, mFilename)));
+        if (fields.empty()) {
             //LOG_ERROR("Cannot read anything from file=%s\n", filename.c_str());
             // probably the file do not exist
         } else {
-            std::sregex_token_iterator first(line.begin(), line.end(), csv_regex, -1);
-            std::sregex_token_iterator last;
-            std::vector<std::string> fields(first, last);
-            //LOG_DEBUG("There are %lu header fields in file=%s\n", fields.size(), filename.c_str());
-            //LOG_DEBUG("header = %s\n", line.c_str());
+            LOG_DEBUG("There are %lu header fields in file=%s\n", fields.size(), filename.c_str());
             mHeader.swap(fields);
             ++mLineNumber;
         }
@@ -62,18 +148,14 @@ std::vector<std::string> CsvParser::GetNext() {
         return dummy;
     }
 
-    std::string line;
-    if (!std::getline(mInFile, line)) {
+    std::vector<std::string> fields(std::move(CsvSplit(mInFile, mFilename)));
+    if (fields.empty()) {
         // nothing more to read
+        LOG_DEBUG("read %d lines from file=%s\n", mLineNumber, mFilename.c_str());
         return dummy;
     }
     // else
-
     ++mLineNumber;
-
-    std::sregex_token_iterator first(line.begin(), line.end(), csv_regex, -1);
-    std::sregex_token_iterator last;
-    std::vector<std::string> fields(first, last);
 
     // max fields is same as that of header
     if (mHasHeader && (fields.size() != mHeader.size())) {
