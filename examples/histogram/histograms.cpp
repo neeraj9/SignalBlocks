af0a8bdbf1259499e1af9cc525a880b1808a6f25
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-03-14 07:28:44 +0530

    the histograms sample application is now evolving into a useful application.

--- examples/histogram/histograms.cpp
+++ examples/histogram/histograms.cpp
@@ -1,12 +1,82 @@
 /// (c) 2016 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>.
 /// All rights reserved.
 
+#include "../../blocks/converters/OrdinalConverter.h"
+#include "../../blocks/basicop/Duplicator.h"
+#include "../../blocks/basicop/Splitter.h"
 #include "../../blocks/sources/CsvFileSource.h"
 #include "../../blocks/sinks/StdoutSink.h"
 #include "../../blocks/sources/SelectiveCsvFileSource.h"
 #include "../../common/logging.h"
 
+#include <sstream>
+
+
 using namespace sigblocks;
+
+
+std::shared_ptr<IPort<std::string> > CreateSimpleHistogramCodeBlocks(const std::string& csvFilename) {
+    std::shared_ptr<IPort<std::string> > source(new CsvFileSource(csvFilename, true));
+    std::shared_ptr<IPort<std::string> > duplicator(new Duplicator<2, std::string>());
+    std::shared_ptr<IPort<std::string> > sink(new StdoutSink<std::string>("[RAW CSV] "));
+
+    std::vector<int> selective_indices;
+    constexpr int num_columns = 4;
+    selective_indices.reserve(num_columns);
+    const std::vector<std::string>& header = dynamic_cast<CsvFileSource*>(source.get())->GetHeader();
+    for (size_t i = 0; i < header.size(); ++i) {
+        if (header[i] == "Country Code" ||
+                header[i] == "Indicator Name" ||
+                header[i] == "Indicator Code" ||
+                header[i] == "2015") {
+            selective_indices.push_back(i);
+        }
+    }
+    std::shared_ptr<IPort<std::string> > splitter(new Splitter<num_columns, std::string>(""));
+
+    // connect as follows:
+    //
+    // source -> duplicator -> sink
+    //               |
+    //               +-------> splitter -> duplicator -> sink
+    //                             |          |
+    //                             |          +--------> ordinalconverter
+    //                             |
+    //                             +-----> duplicator -> sink
+    //                             |          |
+    //                             |          +--------> ordinalconverter
+    //                             |
+    //                             +-----> duplicator -> sink
+    //                             |          |
+    //                             |          +--------> ordinalconverter
+    //                             |
+    //                             +-----> duplicator -> sink
+    //                                        |
+    //                                        +--------> ordinalconverter
+    //
+
+    connect(source, connect(connect(duplicator, sink),
+                            splitter, 1, 0));
+    for (int i = 0; i < num_columns; ++i) {
+        std::ostringstream out;
+        out << "[SINK " << i << "] ";
+        std::shared_ptr<IPort<std::string> > tmpsink(new StdoutSink<std::string>(out.str()));
+        std::shared_ptr<IPort<std::string> > tmpduplicator(new Duplicator<2, std::string>());
+        std::shared_ptr<IPort<std::string> > tmpconverter(new OrdinalConverter);
+        out.str(std::string()); // cleanup and clear contents of out
+        out << "[CONVERTER SINK " << i << "] ";
+        std::shared_ptr<IPort<unsigned long> > tmpconvertersink(new StdoutSink<unsigned long>(out.str()));
+        connect(splitter, connect(connect(tmpduplicator,
+                                          tmpsink),
+                                  tmpconverter,
+                                  1, 0),
+                i, 0);
+        connect(dynamic_cast<OrdinalConverter*>(tmpconverter.get())->GetAsSinkType(), tmpconvertersink);
+    }
+
+    return source;
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -17,9 +87,9 @@ int main(int argc, char* argv[]) {
 
     //std::vector<std::string> select_columns{""};
     //std::shared_ptr<IPort<std::string> > source(new SelectiveCsvFileSource<1>(argv[1], select_columns));
-    std::shared_ptr<IPort<std::string> > source(new CsvFileSource(argv[1], true));
-    std::shared_ptr<IPort<std::string> > sink(new StdoutSink<std::string>());
-    connect(source, sink);
+
+    std::shared_ptr<IPort<std::string> > source(
+            std::move(CreateSimpleHistogramCodeBlocks(argv[1])));
 
     TimeTick time_tick(1);  // always start with non-zero value
     auto pCsvFileSource = dynamic_cast<CsvFileSource*>(source.get());
@@ -27,5 +97,6 @@ int main(int argc, char* argv[]) {
         source->ClockCycle(time_tick);
         time_tick += 1;
     }
+
     return 0;
 }
