72b4bc7b85f31460413b46839cd3588e4fd42863
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-03-15 18:31:05 +0530

    add an enumaration which will be later used while specializing blocks for just scalar, vector or matrix.

---
+++ common/PortType.h
@@ -0,0 +1,15 @@
+//
+// Created by nsharma on 3/15/16.
+//
+
+#ifndef SIGNALBLOCKS_PORTTYPE_H
+#define SIGNALBLOCKS_PORTTYPE_H
+
+namespace sigblocks {
+    enum PortType {
+        PORT_TYPE_SCALAR = 0,
+        PORT_TYPE_VECTOR,
+        PORT_TYPE_MATRIX
+    };
+}
+#endif //SIGNALBLOCKS_PORTTYPE_H
