a35684ac7e01cbb33ca84d43742805a11c6f0bd7
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-04-15 01:01:15 +0530

    Move to header only instead of source, so that any type of go dynamically (at least most of them)

--- blocks/math/BinaryOperator.h
+++ blocks/math/BinaryOperator.h
@@ -5,24 +5,123 @@
 
 #include "../../common/Port.h"
 
-#include <list>
+#include <memory>
+#include <vector>
 
 namespace sigblocks {
     template<class T>
     class BinaryOperator
             : public Port<2, 1, T> {
+    public:
+        BinaryOperator()
+                : mDefaultValue(),
+                  mStorage({mDefaultValue, mDefaultValue}),
+                  mArrayStorage({nullptr, nullptr}),
+                  mDims(),
+                  mLastTick(),
+                  mNumInputsReceived(0) {
+        }
+
+        BinaryOperator(const T& defaultValue)
+                : mDefaultValue(defaultValue),
+                  mStorage({defaultValue, defaultValue}),
+                  mArrayStorage({nullptr, nullptr}),
+                  mDims(),
+                  mLastTick(),
+                  mNumInputsReceived(0) {
+        }
     protected:
-        virtual T Compute(const T& arg1, const T& arg2) const = 0;
+        virtual T ComputeScalar(const T& arg1, const T& arg2) const = 0;
+
+        // compute over vector and store the result in pArg1 (save unnecessary memory allocation
+        virtual void ComputeVector(T* pArg1, T* pArg2, int len) = 0;
+
+        // compute over matrix and store the result in pArg1 (save unnecessary memory allocation
+        virtual void ComputeMatrix(T* pArg1, T* pArg2, const std::vector<int>& dims) = 0;
 
     protected: // Port interface
-        virtual void Process(int sourceIndex, const T& data, const TimeTick& startTime);
+        virtual void Process(int sourceIndex, const T& data, const TimeTick& startTime) {
+            assert(sourceIndex >= 0 || sourceIndex < 2); // XXX change to an assertion library.
+            assert(mDims.empty());
+            if (mLastTick == startTime) {
+                mStorage[sourceIndex] = data;
+                ++mNumInputsReceived;
+            }
+
+            if ((mNumInputsReceived == 2) || ((mNumInputsReceived > 0) && (mLastTick != startTime))) {
+                // time to push out the data
+                T output = this->ComputeScalar(mStorage[0], mStorage[1]);
+                LeakData(0, output, mLastTick);
+                mNumInputsReceived = 0;
+                mStorage[0] = mDefaultValue;
+                mStorage[1] = mDefaultValue;
+            }
+            if (mLastTick != startTime) {
+                mLastTick = startTime;
+                mStorage[sourceIndex] = data;
+            }
+        }
 
         virtual void Process(
-                int sourceIndex, std::unique_ptr<T[]> data, int len, const TimeTick& startTime);
+                int sourceIndex, std::unique_ptr<T[]> data, int len, const TimeTick& startTime) {
+            assert(sourceIndex >= 0 || sourceIndex < 2); // XXX change to an assertion library.
+            assert(mNumInputsReceived == 0);
+            if (! mDims.empty()) {
+                mDims.push_back(len);
+            } else {
+                assert(len == mDims[0]);
+                assert(mDims.size() == 1);
+            }
+            if (mLastTick == startTime) {
+                mArrayStorage[sourceIndex].swap(data);
+            }
+            if (((mArrayStorage[0]) && (mArrayStorage[1])) || ((mArrayStorage[0] || mArrayStorage[1]) && (mLastTick != startTime))) {
+                // done, so transmit now
+                this->ComputeVector(mArrayStorage[0].get(), mArrayStorage[1].get(), len);
+                this->LeakData(0, std::move(mArrayStorage[0]), len, mLastTick);
+                mArrayStorage[0].reset(nullptr);
+                mArrayStorage[1].reset(nullptr);
+                mDims.clear();
+            }
+            if (mLastTick != startTime) {
+                mLastTick = startTime;
+                mArrayStorage[sourceIndex].swap(data);
+            }
+        }
+
+        virtual void ProcessMatrix(
+                int sourceIndex, std::unique_ptr<T[]> data, const std::vector<int>& dims, const TimeTick& startTime) {
+            assert(sourceIndex >= 0 || sourceIndex < 2); // XXX change to an assertion library.
+            assert(mNumInputsReceived == 0);
+            if (! mDims.empty()) {
+                mDims = dims;
+            } else {
+                assert(dims == mDims);
+            }
+            if (mLastTick == startTime) {
+                mArrayStorage[sourceIndex].swap(data);
+            }
+            if (((mArrayStorage[0]) && (mArrayStorage[1])) || ((mArrayStorage[0] || mArrayStorage[1]) && (mLastTick != startTime))) {
+                // done, so transmit now
+                this->ComputeMatrix(mArrayStorage[0].get(), mArrayStorage[1].get(), dims);
+                this->LeakMatrix(0, std::move(mArrayStorage[0]), dims, mLastTick);
+                mArrayStorage[0].reset(nullptr);
+                mArrayStorage[1].reset(nullptr);
+                mDims.clear();
+            }
+            if (mLastTick != startTime) {
+                mLastTick = startTime;
+                mArrayStorage[sourceIndex].swap(data);
+            }
+        }
 
     private:
-        std::list<T> mDataPort[2];
-        std::list<TimeTick> mDataPortTime[2];
+        T mDefaultValue;
+        T mStorage[2];  ///< for scalar types
+        std::unique_ptr<T[]> mArrayStorage[2]; ///< for vector and matrix types
+        std::vector<int> mDims;  ///< for vector and matrix types
+        TimeTick mLastTick;
+        int mNumInputsReceived;
     };
 }
 
