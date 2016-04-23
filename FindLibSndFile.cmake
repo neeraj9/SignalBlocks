5d7e27d7cddcd08d3d771677308679808c93102c
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-04-23 11:16:28 +0530

    Start working on integrating libsndfile before writing a more complete audio source which can support multiple audio file formats. Huh! no mp3 support yet because of licensing issues.

---
+++ FindLibSndFile.cmake
@@ -0,0 +1,40 @@
+# (c) 2016 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>.
+# All rights reserved.
+
+# - Try to find libsndfile library and headers
+# Once done, this will define
+#
+#  LIBSNDFILE_FOUND - system has libsndfile
+#  LIBSNDFILE_INCLUDE_DIR - the libsndfile include directories
+#  LIBSNDFILE_LIBRARIES - link these to use libsndfile
+
+
+if (LIBSNDFILE_INCLUDE_DIR AND LIBSNDFILE_LIBRARIES)
+    set(LIBSNDFILE_FIND_QUIETLY TRUE)
+endif (LIBSNDFILE_INCLUDE_DIR AND LIBSNDFILE_LIBRARIES)
+
+# Include dir
+find_path(LIBSNDFILE_INCLUDE_DIR
+        NAMES sndfile.h
+        )
+
+# Library
+find_library(LIBSNDFILE_LIBRARY
+        NAMES sndfile
+        )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIBSNDFILE DEFAULT_MSG LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
+
+# If we successfully found the sndfile library then add the library to the
+# LIBSNDFILE_LIBRARIES cmake variable otherwise set LIBSNDFILE_LIBRARIES to nothing.
+if(LIBSNDFILE_FOUND)
+    set(LIBSNDFILE_LIBRARIES ${LIBSNDFILE_LIBRARY})
+else(LIBSNDFILE_FOUND)
+    set(LIBSNDFILE_LIBRARIES)
+endif(LIBSNDFILE_FOUND)
+
+
+# Lastly make it so that the LIBSNDFILE_LIBRARY and LIBSNDFILE_INCLUDE_DIR variables
+# only show up under the advanced options in the cmake applications.
+MARK_AS_ADVANCED(LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
