8afe1cdb601a7564ee1cc6a097abc233486fc4ef
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-04-25 19:03:45 +0530

    Fix AudioSource after change in way dimensions are passed for matrix. Additionally, rewind the source correctly so that the system will seamlessly loop the same audio source.

--- blocks/sources/AudioSource.h
+++ blocks/sources/AudioSource.h
@@ -16,8 +16,8 @@
 namespace sigblocks {
 
     /// This block generates matrix (2-D) output only, where the first
-    /// dimension is the number of channels and second dimension is
-    /// is the block size provided in the constructor.
+    /// dimension is the block size and the second dimension is the
+    /// number of channels.
     ///
     /// The number of data types supported is limited to the following:
     /// * char
@@ -34,11 +34,16 @@ namespace sigblocks {
     class AudioSource
             : public Port<0, 1, T> {
     public:
-        AudioSource(int blockSize, std::unique_ptr<std::istream> pIns)
+        /// set enableNormalisation to false when you dont want
+        /// integer audio samples to be normalised to [-1.0, 1.0]
+        /// when read as float or double.
+        AudioSource(int blockSize,
+                    std::unique_ptr<std::istream> pIns,
+                    bool enableNormalisation = true)
                 : mLastTick(),
                   mLoopOver(true),
                   mBlockSize(blockSize),
-                  mSoundInfo() {
+                  mSoundInfo(enableNormalisation) {
 
             bool success = mSoundInfo.OpenStream(std::move(pIns));
             if (!success) {
@@ -68,10 +73,26 @@ namespace sigblocks {
             std::unique_ptr<T[]> data(new T[max_elements]);
 
             int elements_read = static_cast<int>(mSoundInfo.Read(data.get(), max_elements));
-            // the samples read are one for each channel and then next frame (or sample for
-            // all channels), and so on.
-            std::vector<int> dims = {mSoundInfo.GetNumChannels(),
-                                     elements_read / mSoundInfo.GetNumChannels()};
+            if (elements_read <= 0) {
+                // lets try again if looping is enabled
+                if (mSoundInfo.IsEof() && mLoopOver) {
+                    mSoundInfo.Rewind();
+                    // lets try again
+                    elements_read = static_cast<int>(mSoundInfo.Read(data.get(), max_elements));
+                    if (elements_read <= 0) {
+                        // still doesnt work, so give up
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+            // The samples read are one for each channel and then next frame (or sample for
+            // all channels), and so on. That is [{ch1, ch2}, {ch1, ch2}, ...] or
+            // alternatively as C/C++ 2-d array:
+            // T samples[num_samples][num_channels]
+            std::vector<int> dims = {elements_read / mSoundInfo.GetNumChannels(),
+                                     mSoundInfo.GetNumChannels()};
             this->LeakMatrix(0, std::move(data), dims, timeTick);
             if (mSoundInfo.IsEof() && mLoopOver) {
                 mSoundInfo.Rewind();
