f06a1bffe246477589e6624b49555ca667a08c3e
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-04-26 15:15:38 +0530

    move back to C++11, since that is enough at present.

--- compiler.cmake
+++ compiler.cmake
@@ -8,11 +8,11 @@
 # make reorder warning to failure
 set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Werror=reorder -Werror=sign-compare")
 
-if (USE_GNU_CXX14_BUILD)
+if (USE_GNU_CXX11_BUILD)
 
     # ------------ option-2: use gnu g++ with libstdc++ ------
     # You can always revert to libstdc++ when using gnu c++
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
 else()
 
@@ -30,7 +30,7 @@ else()
         # the default compiler is c++, which is typically g++ (gnu c++ compiler)
         # lets use clang
         set(CMAKE_CXX_COMPILER           "/opt/storage/2/nsharma/llvm/bin/clang++")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -stdlib=libc++")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
         # if we dont provide -lc++abi then the following error is displayed
         # libc++abi.so.1, needed by /opt/storage/2/nsharma/llvm/bin/../lib64/libc++.so
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++abi")
@@ -45,8 +45,8 @@ else()
 
         # ------------ option-3: use llvm+clang with libstdc++ ------
         set(CMAKE_CXX_COMPILER           "/opt/storage/2/nsharma/llvm/bin/clang++")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
     endif(USE_LLVM_WITH_LIBCXX_LIBCXXABI)
 
-endif(USE_GNU_CXX14_BUILD)
+endif(USE_GNU_CXX11_BUILD)
