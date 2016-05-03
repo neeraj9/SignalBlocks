2dd0cfd759b1577c8f2445a394f9b8b2de247ac3
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2016-05-03 07:13:37 +0530

    The correct way of using license is to embed as suggested by http://www.gnu.org/licenses/gpl-howto.en.html

--- test/test_blocks_math_tan.cpp
+++ test/test_blocks_math_tan.cpp
@@ -1,5 +1,19 @@
-// (c) 2016 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
-// see LICENSE for license
+// (c) 2016 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>.
+// This file is part of SignalBlocks.
+//
+// SignalBlocks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SignalBlocks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SignalBlocks.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #include "../common/Port.h"
 #include "../blocks/math/Tan.h"
