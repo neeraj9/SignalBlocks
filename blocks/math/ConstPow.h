1d876d22003438793b681e701cc5fcee3587d6ed
Neeraj Sharma <neerajsharma.9@outlook.com>
2019-04-03 22:33:40 +0530

    remove gsl optimizations when its gone

--- blocks/math/ConstPow.h
+++ blocks/math/ConstPow.h
@@ -38,50 +38,6 @@ namespace signalblocks {
         }
 
     };
-
-    // Partial template specialization for optimized
-    // power function.
-    //
-    // The c-equivalent functions are not optimized for
-    // lower power, so template specialization is employed
-    // to take advantage of specific gsl functions.
-    //
-    // Note to improve efficiency these
-    // functions do not check for overflow or underflow conditions.
-    //
-    // computes  x^P, where P is a constant
-#define OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(P) \
-  template <class T> \
-  class ConstPow<T, P> \
-    : public UnaryOperator<T> \
-  { \
-  public: \
-    ConstPow(std::string name) \
-        : UnaryOperator<T>(std::move(name)) { \
-    } \
-  protected: \
-    virtual T Compute(const T& arg1) \
-    { \
-      return gsl_pow_##P(arg1); \
-    } \
-  };
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(2);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(3);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(4);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(5);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(6);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(7);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(8);
-
-    OPTIMIZED_POW_TEMPLATE_SPECIALIZATION(9);
-
 }
 
 #endif // SIGNALBLOCKS_CONSTPOW_H
